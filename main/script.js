0893c9155ea10b4b1d6b79fd9363dafdd912803e
rmochaoa <rmochaoa023@student.wethinkcode.co.za>
2024-09-26 11:28:33 +0200

    scramble class reinstatiating

---
+++ main/script.js
@@ -0,0 +1,7 @@
+class TextScramble {
+    constructor(el) {
+        this.el = el;
+        this.chars = '!<>-_\\/[]{}—=+*^?#________';
+        this.update = this.update.bind(this);
+        this.isAnimating = false;
+    }
